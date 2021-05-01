c2065beba466506951d41fb6a6daf7e4acfd8d73
skattt12345 <vector150150@gmail.com>
2021-05-01 19:17:37 +0300

    prepros

---
+++ #source/js/script.js
@@ -0,0 +1,33 @@
+console.log(123);
+class Animal{
+
+
+	constructor(name,color){
+		this.name = name;
+		this.color = color;
+	}
+	showAnimal(){
+		console.log("My name is "this.name " and my color is"this.color)
+	}
+
+}
+
+class Zoo{
+	animalZoo = [];
+
+	constructor(){
+
+	}
+	addZoo(){
+		this.animalZoo.push("bear")
+	}
+	deleteZoo(){
+		this.animalZoo.pop()
+	}
+}
+
+const cat = new Animal("cat","black");
+const bear = new Animal("bear","brown")
+const animals = new Zoo();
+animals.addZoo(cat)
+animals.addZoo(bear)
